47746421678f331c27b216354957fca2b45a6c87
chinanf-boy <865501259@qq.com>
2019-04-06 20:05:10 +0800

    js: change to two srt burn to mp4

--- get_p.js
+++ get_p.js
@@ -2,6 +2,24 @@ const fs = require('fs');
 
 const data = fs.readFileSync('../.list', 'utf-8');
 
+const zhStyle = f =>
+  `subtitles=${f}:force_style='Fontsize=15,MarginV=18,BorderStyle=1,Outline=1,Shadow=0,PrimaryColour=&HFFFFFF&,OutlineColour=&H853F1B&,Spacing=3'`;
+const enStyle = f =>
+  `subtitles=${f}:force_style='Fontsize=8,MarginV=8,BorderStyle=0,Outline=1,Shadow=0,PrimaryColour=&HFFFFFF&,OutlineColour=&H5A6A83&,Spacing=1'`;
+
+function addPrefix(filename, en = false) {
+  if (en) {
+    return `./srts/${filename}.srt`;
+  }
+  return `./srts/${filename}.zh.srt`;
+}
+
+const srts = index => {
+  return `${zhStyle(addPrefix(two(index)))},${enStyle(
+    addPrefix(two(index), true)
+  )}`;
+};
+
 let arrData = data.split('\n');
 let Obj = {};
 arrData.forEach(d => {
@@ -27,13 +45,19 @@ const addZh = str => {
 
 // console.log(Obj);
 
-console.log('# 由 `node get_p.js > video_add_sub.sh` 生成')
+console.log('# 由 `node get_p.js > video_add_sub.sh` 生成');
+
+let Rs = {};
 
 Object.keys(Obj).forEach(index => {
   index &&
-    console.log(
-      `ffmpeg -i "../Source/${Obj[index]}" -vf subtitles="./srts/${two(
-        index
-      )}.zh.srt" "../Zh/${addZh(Obj[index])}"`
-    );
+    (Rs[two(index)] = `ffmpeg -y -i "../Source/${Obj[index]}" -vf "${srts(
+      index
+    )}" "../Zh/${addZh(Obj[index])}"`);
 });
+
+if (process.argv[3]) {
+  Obejct.keys(Rs).forEach(r => console.log(r));
+} else if (process.argv[2]) {
+  console.log(Rs[two('P' + process.argv[2])]);
+}
